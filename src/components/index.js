0c7c2eca448ff2e3975f5e2622e187cfec7ddb2e
Yulier Camilo Garrido Garrido <ycgarrido@gmail.com>
2018-07-11 12:55:58 -0400

    Create copyright component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
+import Copyright from "./Copyright";
 import Hero from "./Hero";
 
 export {
+	Copyright,
 	Hero
 };
---
+++ src/stories/components/copyright/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {withKnobs, text, number, boolean, array} from '@storybook/addon-knobs/react';
+import withReadme from 'storybook-readme/with-readme';
+import {ThemeSelector} from "../../../addons/ThemeSwitcher";
+
+import {Copyright} from '../../../components';
+import Readme from '../../../components/Copyright/README.md';
+
+const stories = storiesOf('Components', module);
+
+stories.addDecorator(withKnobs);
+
+stories.add('Copyright', withReadme([Readme], () => {
+	return (
+		<ThemeSelector>
+			<Copyright
+				showAllRightText={boolean("Show all right text", true)}
+				showCopyRightSymbol={boolean("Show copy right symbol", true)}
+				showCopyRightText={boolean("Show copy right text", true)}
+				showYear={boolean("Show year", true)}
+				allRightText={text("All right text", "All rights reserved")}
+				copyRightText={text("Copy right text", "Copyright")}
+				text={text("Text", "Fron10, inc")}
+			/>
+		</ThemeSelector>
+	);
+}));

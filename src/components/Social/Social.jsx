1d29fc8e7d5ea1556fd3e28c80ae19edea00fcdf
Yulier Camilo Garrido Garrido <ycgarrido@gmail.com>
2018-07-13 09:25:39 -0400

    Fix Social buttons

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -37,144 +37,105 @@ class Social extends Component {
 		const {
 			url,
 			rounded,
-			facebook,
-			google,
-			linkedin,
-			twitter,
-			telegram,
-			whatsapp,
-			pinterest,
-			vk,
-			ok,
-			reddit,
-			tumblr,
-			livejournal,
-			viber,
-			email,
+			type
 		} = this.props;
-		return <div className="Social">
+		return <React.Fragment>
 			{
-				facebook &&
+				type === 'facebook' &&
 				<FacebookShareButton url={url}>
 					<FacebookIcon round={rounded}/>
 				</FacebookShareButton>
 			}
 			{
-				google &&
+				type === 'google' &&
 				<GooglePlusShareButton url={url}>
 					<GooglePlusIcon round={rounded}/>
 				</GooglePlusShareButton>
 			}
 			{
-				linkedin &&
+				type === 'linkedin' &&
 				<LinkedinShareButton url={url}>
 					<LinkedinIcon round={rounded}/>
 				</LinkedinShareButton>
 			}
 			{
-				twitter &&
+				type === 'twitter' &&
 				<TwitterShareButton url={url}>
 					<TwitterIcon round={rounded}/>
 				</TwitterShareButton>
 			}
 			{
-				telegram &&
+				type === 'telegram' &&
 				<TelegramShareButton url={url}>
 					<TelegramIcon round={rounded}/>
 				</TelegramShareButton>
 			}
 			{
-				whatsapp &&
+				type === 'whatsapp' &&
 				<WhatsappShareButton url={url}>
 					<WhatsappIcon round={rounded}/>
 				</WhatsappShareButton>
 			}
 			{
-				pinterest &&
+				type === 'pinterest' &&
 				<PinterestShareButton url={url}
 				                      media="Share in Pinterest">
 					<PinterestIcon round={rounded}/>
 				</PinterestShareButton>
 			}
 			{
-				vk &&
+				type === 'vk' &&
 				<VKShareButton url={url}>
 					<VKIcon round={rounded}/>
 				</VKShareButton>
 			}
 			{
-				ok &&
+				type === 'ok' &&
 				<OKShareButton url={url}>
 					<OKIcon round={rounded}/>
 				</OKShareButton>
 			}
 			{
-				reddit &&
+				type === 'reddit' &&
 				<RedditShareButton url={url}>
 					<RedditIcon round={rounded}/>
 				</RedditShareButton>
 			}
 			{
-				tumblr &&
+				type === 'tumblr' &&
 				<TumblrShareButton url={url}>
 					<TumblrIcon round={rounded}/>
 				</TumblrShareButton>
 			}
 			{
-				livejournal &&
+				type === 'livejournal' &&
 				<LivejournalShareButton url={url}>
 					<LivejournalIcon round={rounded}/>
 				</LivejournalShareButton>
 			}
 			{
-				viber &&
+				type === 'viber' &&
 				<ViberShareButton url={url}>
 					<ViberIcon round={rounded}/>
 				</ViberShareButton>
 			}
 			{
-				email &&
+				type === 'email' &&
 				<EmailShareButton url={url}>
 					<EmailIcon round={rounded}/>
 				</EmailShareButton>
 			}
-		</div>
+		</React.Fragment>
 	}
 }
 
 Social.propTypes = {
-	facebook: PropTypes.bool,
-	google: PropTypes.bool,
-	linkedin: PropTypes.bool,
-	twitter: PropTypes.bool,
-	telegram: PropTypes.bool,
-	whatsapp: PropTypes.bool,
-	pinterest: PropTypes.bool,
-	vk: PropTypes.bool,
-	ok: PropTypes.bool,
-	reddit: PropTypes.bool,
-	tumblr: PropTypes.bool,
-	livejournal: PropTypes.bool,
-	viber: PropTypes.bool,
-	email: PropTypes.bool,
+	type: PropTypes.string,
 	rounded: PropTypes.bool,
 	url: PropTypes.string,
 };
 Social.defaultProps = {
-	facebook: true,
-	google: true,
-	linkedin: true,
-	twitter: true,
-	telegram: true,
-	whatsapp: true,
-	pinterest: true,
-	vk: true,
-	ok: true,
-	reddit: true,
-	tumblr: true,
-	livejournal: true,
-	viber: true,
-	email: true,
+	type: "email",
 	rounded: true,
 	url: ""
 };

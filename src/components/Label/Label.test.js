b6d5cecb9c3949c568ee63c50f2ba9ab05dddb18
Yoandry <systems@front10.com>
2018-07-30 11:25:07 -0400

    working on eslint errors

--- src/components/Label/Label.test.js
+++ src/components/Label/Label.test.js
@@ -5,28 +5,27 @@ import Label from './Label';
 
 configure({ adapter: new Adapter() });
 
-describe("Label", () => {
-	let props;
-	let mounted;
-	const label = () => {
-		if (!mounted) {
-			mounted = mount(<Label {...props} />);
-		}
-		return mounted;
-	};
-	describe('Label', () => {
+describe('Label', () => {
+  let props;
+  let mounted;
+  const label = () => {
+    if (!mounted) {
+      mounted = mount(<Label {...props} />);
+    }
+    return mounted;
+  };
+  describe('Label', () => {
+    beforeEach(() => {
+      props = {};
+      mounted = undefined;
+    });
 
-		beforeEach(() => {
-			props = {};
-			mounted = undefined;
-		});
-
-		it('should render', () => {
-			expect(mount(<Label {...props} />)).toMatchSnapshot();
-		});
-		it("always renders a div", () => {
-			const divs = label().find("label");
-			expect(divs.length).toBeGreaterThan(0);
-		});
-	});
-})
+    it('should render', () => {
+      expect(mount(<Label {...props} />)).toMatchSnapshot();
+    });
+    it('always renders a div', () => {
+      const divs = label().find('label');
+      expect(divs.length).toBeGreaterThan(0);
+    });
+  });
+});

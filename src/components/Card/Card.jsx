1aaddceaf3fde16be0770f19c81e164123641fc8
Yoandry <systems@front10.com>
2018-08-02 14:33:12 -0400

    working on eslint rules

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Card extends Component {
-    constructor(props) {
-        super(props);        
-    }
+class Card extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
 		const {
 			image,
@@ -17,33 +18,38 @@ class Card extends Component {
 			contentAlign,
 			children
 		} = this.props;
-		return <div className={`card h-100 Card ${!showBorder ? 'border-0' : ''}`}>
-			<div className={`card-body text-${contentAlign}`}>
-				{
-					image &&
-					<img className={`${imageCircle ? 'rounded-circle' : ''} ${imageBorder ? 'img-thumbnail' : ''} img-fluid`} src={image}
-					     alt="Generic placeholder image"/>
-				}
-				{
-					title &&
-					<div className="Card__Title mb-2">{title}</div>
-				}
-				{
-					subTitle &&
-					<div className="Card__Subtitle mb-3">{subTitle}</div>
-				}
-				{
-					summary &&
-					<p className="Card__Summary">{summary}</p>
-				}
-				{
-					children &&
-					<React.Fragment>
-						{children}
-					</React.Fragment>
-				}
+		return (
+			<div className={`card h-100 Card ${!showBorder ? 'border-0'	: ''}`}>
+				<div className={`card-body text-${contentAlign}`}>
+					{image && (<img
+						className={`${imageCircle
+							? 'rounded-circle'
+							: ''} ${imageBorder
+								? 'img-thumbnail'
+								: ''} img-fluid`}
+						src={image}
+						alt="Generic placeholder image" />))}
+																				{title && <div className="Card__Title mb-2">
+						{title}
+					</div>
+					}
+					{subTitle && <div className="Card__Subtitle mb-3">
+						{subTitle}
+					</div>
+					}
+					{summary && <p className="Card__Summary">
+						{summary}
+					</p>
+					}
+					{children && (
+						<React.Fragment>
+							{children}
+						</React.Fragment>
+					)
+					}
+				</div>
 			</div>
-		</div>
+		);
 	}
 }
 
@@ -55,17 +61,17 @@ Card.propTypes = {
 	title: PropTypes.string,
 	subTitle: PropTypes.string,
 	summary: PropTypes.string,
-	contentAlign: PropTypes.string,
+	contentAlign: PropTypes.string
 };
 Card.defaultProps = {
 	showBorder: true,
 	imageCircle: true,
 	imageBorder: false,
-	image: "",
-	title: "",
-	subTitle: "",
-	summary: "",
-	contentAlign: "center"
+	image: '',
+	title: '',
+	subTitle: '',
+	summary: '',
+	contentAlign: 'center'
 };
 
 export default Card;
--- src/stories/documentation/Introduction/index.js
+++ src/stories/documentation/Introduction/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 // import Markdown from 'wix-storybook-utils/Markdown';
 import Markdown from '../../../../storybook-utils/components/Markdown';
-import Readme from './../../../../README.md';
+import Readme from '../../../../README.md';
 
-storiesOf('Introduction', module).add('Getting Started', () => {
-  return <Markdown source={Readme} />;
-});
+storiesOf('Introduction', module).add('Getting Started', () => <Markdown source={Readme} />);

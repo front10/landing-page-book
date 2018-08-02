47f2743b577c5c33cbae46756554eb066048a80c
Yulier Camilo Garrido Garrido <ycgarrido@gmail.com>
2018-08-02 14:11:18 -0400

    Create ContactInfo

---
+++ src/stories/components/contact-info/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {withKnobs, text, select, boolean, number} from '@storybook/addon-knobs/react';
+import withReadme from 'storybook-readme/with-readme';
+import {ThemeSelector} from '../../../addons/ThemeSwitcher';
+
+import {ContactInfo, Row, Column} from '../../../components';
+import Readme from '../../../components/ContactInfo/README.md';
+
+const stories = storiesOf('Components', module);
+
+stories.addDecorator(withKnobs);
+
+stories.add('Contact info', withReadme([Readme], () => {
+  return (
+    <ThemeSelector>
+      <Row>
+        <Column className="col col-md-4">
+          <ContactInfo
+            showBorder={boolean("Show border", true)}
+            showIcons={boolean("Show icons", true)}
+            lat={number("Latitude", 26.5)}
+            lng={number("Longitude", -80.2)}
+            locationApiKey={text("Location api key", "AIzaSyAP5ycGdpjGBUU-u6vZ8fwx_ybbyzgYXfM")}
+            address={text("Address", "9728 Birchwood St. Navarre, FL 32566")}
+            email={text("Email", "contact@front10.com")}
+            phone={text("Phone", "+001 3521 4587")}
+            mobile={text("Mobile", "+001 3521 4587")}
+            fax={text("Fax", "208-658744")}
+            website={text("Website", "www.front10.com")}
+            contentAlign={select("Content align", {"left": "left", "center": "center", "right": "right"}, "left")}
+          />
+        </Column>
+      </Row>
+    </ThemeSelector>
+  );
+}));

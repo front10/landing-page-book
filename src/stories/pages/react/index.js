a434b5b06b558fd6979f03f0e298bb0d46a399d9
Dariel Noel Vila <darielnoel@gmail.com>
2018-08-08 22:58:25 -0400

    Remove unnecesary code and adds basic links to the navbar.

--- src/stories/pages/react/index.js
+++ src/stories/pages/react/index.js
@@ -5,76 +5,15 @@ import { ThemeSelector } from '../../../addons/ThemeSwitcher';
 
 import {
   Navbar,
+  NavbarBrand,
+  NavbarNav,
+  NavbarCollapse,
+  NavbarLink,
+  Icon,
+  Image,
   Hero,
-  Section,
-  Video,
-  Team,
-  Container,
-  BuiltWith,
-  ContactUs
+  Container
 } from '../../../components';
-
-const leftItems = [
-  {
-    title: 'Docs',
-    href: 'https://facebook.com'
-  },
-  {
-    title: 'Tutorial'
-  },
-  {
-    title: 'Community'
-  },
-  {
-    title: 'Blog'
-  }
-];
-
-const services = [
-  {
-    image: 'images/demo/default.svg',
-    name: 'Component libraries',
-    summary: 'React & AngularJs reusable component libraries creation.'
-  },
-  {
-    image: 'images/demo/default.svg',
-    name: 'Project acceleration',
-    summary: 'Help you to accelerate and scale your Front-End projects.'
-  },
-  {
-    image: 'images/demo/default.svg',
-    name: 'Consulting',
-    summary: 'More than 10 years of experience doing Front-End development.'
-  }
-];
-
-const team = [
-  {
-    image: 'https://front10.com/img/team/dariel.jpeg',
-    name: 'Dariel Vila',
-    job: 'Co-Founder'
-  },
-  {
-    image: 'https://front10.com/img/team/albe.jpeg',
-    name: 'Alberto Roman',
-    job: 'Co-Founder'
-  }
-];
-
-const frameworks = [
-  {
-    image: 'images/frameworks/react.png'
-  },
-  {
-    image: 'images/frameworks/angularjs.png'
-  },
-  {
-    image: 'images/frameworks/graphql.png'
-  },
-  {
-    image: 'images/frameworks/storybook.png'
-  }
-];
 
 const buttons = [
   {
@@ -93,53 +32,44 @@ stories.addDecorator(withKnobs);
 
 stories.add('React', () => (
   <ThemeSelector>
-    <Navbar
-      companyName="React"
-      companyLink="https://reactjs.org/"
-      companyLogo="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
-      leftItems={leftItems}
-      expand="md"
-    />
+    <Navbar expand="md">
+      <Container>
+        <NavbarCollapse>
+          <NavbarBrand href="https://reactjs.org/">
+            <Image
+              width="24"
+              src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+              alt="React logo"
+            />
+            React
+          </NavbarBrand>
+          <NavbarNav alignItems="right">
+            <NavbarLink href="https://reactjs.org/docs/getting-started.html">Docs</NavbarLink>
+            <NavbarLink href="https://reactjs.org/tutorial/tutorial.html">Tutorial</NavbarLink>
+            <NavbarLink href="https://reactjs.org/community/support.html">Community</NavbarLink>
+            <NavbarLink href="https://reactjs.org/blog/2018/08/01/react-v-16-4-2.html">
+              Blog
+            </NavbarLink>
+            <NavbarLink>
+              <Icon icon="fa fa-search" />
+            </NavbarLink>
+            <NavbarLink>v16.4.2</NavbarLink>
+            <NavbarLink>GitHub</NavbarLink>
+            <NavbarLink>
+              <Icon icon="fa fa-share" />
+            </NavbarLink>
+          </NavbarNav>
+        </NavbarCollapse>
+      </Container>
+    </Navbar>
     <Hero
       overlayColor="#283A43"
-      particles
+      particles={false}
       opacity={1}
       subHeaderPosition="bottom"
       subHeader="A JavaScript library for building user interfaces"
       header="React"
       buttons={buttons}
     />
-    <div className="mt-5">
-      <Section title="OUR SERVICES" subTitle="We do scalable Front-End development 3x faster.">
-        <Team members={services} imageBorder showBorder={false} />
-      </Section>
-      <Section gray title="WHAT WE DO" subTitle="One Look Is Worth A Thousand Words.">
-        <Video source="https://youtu.be/dCrLwWdju68" />
-      </Section>
-      <Section title="OUR FOUNDERS" subTitle="Dariel & Alberto.">
-        <Container>
-          <Team
-            members={team}
-            imageBorder
-            showBorder={false}
-            socials={['twitter', 'facebook', 'linkedin']}
-            socialGray
-          />
-        </Container>
-      </Section>
-      <Section gray>
-        <Container>
-          <BuiltWith companies={frameworks} gray />
-        </Container>
-      </Section>
-      <Hero
-        isFixed={false}
-        image="images/hero/map-image.png"
-        particles={false}
-        backgroundColor="#383d45"
-      >
-        <ContactUs />
-      </Hero>
-    </div>
   </ThemeSelector>
 ));

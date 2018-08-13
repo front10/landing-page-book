3bb10f297c904d82896e346a218333df42644e6b
Yoandry <systems@front10.com>
2018-08-13 14:58:45 -0400

    GraphQL page  90%

--- src/stories/pages/graphql/index.js
+++ src/stories/pages/graphql/index.js
@@ -20,21 +20,30 @@ import {
   Image,
   Row,
   Column,
-  Code
+  Code,
+  Video,
+  Footer,
+  Copyright
 } from '../../../components';
 
 const frameworks = [
   {
-    image: 'images/frameworks/react.png'
+    image: 'https://graphql.org/users/logos/facebook.png'
   },
   {
-    image: 'images/frameworks/angularjs.png'
+    image: 'https://graphql.org/users/logos/github.png'
   },
   {
-    image: 'images/frameworks/graphql.png'
+    image: 'https://graphql.org/users/logos/pinterest.png'
   },
   {
-    image: 'images/frameworks/storybook.png'
+    image: 'https://graphql.org/users/logos/intuit.png'
+  },
+  {
+    image: 'https://graphql.org/users/logos/coursera.png'
+  },
+  {
+    image: 'https://graphql.org/users/logos/shopify.png'
   }
 ];
 
@@ -152,9 +161,9 @@ stories.add('GraphQl', () => (
             </Column>
             <Column />
           </Row>
-          <Row className="row mt-5 mb-5 First_section">
+          <Row className="row mt-5 mb-5 Code_section vertical">
             <Column />
-            <Column className="col col-md-3 col-ms-6 col-xs-12">
+            <Column className="col col-md-4 col-ms-6 col-xs-12">
               <div className="Code_container">
                 <GraphqlCodeBlock
                   className="GraphqlCodeBlock"
@@ -169,7 +178,7 @@ stories.add('GraphQl', () => (
                 <JSONPretty id="json-pretty" json={graphqlJson} />
               </div>
             </Column>
-            <Column className="col col-md-3 col-sm-6 col-xs-12">
+            <Column className="col col-md-4 col-sm-6 col-xs-12">
               <br />
               <h3>Ask for what you need, get exactly that</h3>
               <br />
@@ -182,13 +191,253 @@ stories.add('GraphQl', () => (
             <Column />
           </Row>
         </Section>
-        <Section gray title="WHAT WE DO" subTitle="One Look Is Worth A Thousand Words." />
-        <Section title="OUR FOUNDERS" subTitle="Dariel & Alberto." />
         <Section gray>
+          <Row>
+            <Column className="col col-md-4 col-sm-6 col-xs-12">
+              <br />
+              <h3>Get many resources in a single request</h3>
+              <br />
+              <p>
+                GraphQL queries access not just the properties of one resource but also smoothly
+                follow references between them. While typical REST APIs require loading from
+                multiple URLs, GraphQL APIs get all the data your app needs in a single request.
+                Apps using GraphQL can be quick even on slow mobile network connections.
+              </p>
+            </Column>
+            <Column className="col col-md-4 col-sm-6 col-xs-12">
+              <h6>Query & Response animation here</h6>
+            </Column>
+          </Row>
+        </Section>
+        <Section>
+          <Row className="row mt-5 mb-5 Code_section_vertical">
+            <Column className="col col-md-6 col-sm-6 col-xs-12">
+              <div className="Code_container">
+                <GraphqlCodeBlock
+                  className="GraphqlCodeBlock"
+                  queryBody="{
+                      hero {
+                        name
+                        height
+                        mass
+                      }
+                    }"
+                />
+                <GraphqlCodeBlock
+                  className="GraphqlCodeBlock border_separator"
+                  queryBody="type Query {
+                    hero: Character
+                  }
+                  
+                  type Character {
+                    name: String
+                    friends: [Character]
+                    homeWorld: Planet
+                    species: Species
+                  }
+                  
+                  type Planet {
+                    name: String
+                    climate: String
+                  }
+                  
+                  type Species {
+                    name: String
+                    lifespan: Int
+                    origin: Planet
+                  }"
+                />
+              </div>
+            </Column>
+            <Column className="col col-md-4 col-sm-6 col-xs-12">
+              <br />
+              <h3>Get many resources in a single request</h3>
+              <br />
+              <p>
+                GraphQL queries access not just the properties of one resource but also smoothly
+                follow references between them. While typical REST APIs require loading from
+                multiple URLs, GraphQL APIs get all the data your app needs in a single request.
+                Apps using GraphQL can be quick even on slow mobile network connections.
+              </p>
+            </Column>
+          </Row>
+        </Section>
+        <Section id="dark-section" title="Move faster with powerful developer tools">
+          <p>
+            Know exactly what data you can request from your API without leaving your editor,
+            highlight potential issues before sending a query, and take advantage of improved code
+            intelligence. GraphQL makes it easy to build powerful tools like GraphiQL by leveraging
+            your API’s type system.
+          </p>
+          <Video
+            source="https://graphql.org/img/graphiql.mp4"
+            playing
+            loop
+            controls
+            width="640px"
+            height="360px"
+          />
+        </Section>
+        <Section gray>
+          <Row className="row mt-5 mb-5 Code_section_vertical">
+            <Column className="col col-md-6 col-sm-6 col-xs-12">
+              <div className="Code_container">
+                <GraphqlCodeBlock
+                  className="GraphqlCodeBlock"
+                  queryBody="{
+                    hero {
+                      name
+                      height
+                      mass
+                    }
+                  }"
+                />
+                <GraphqlCodeBlock
+                  className="GraphqlCodeBlock border_separator"
+                  queryBody="type Query {
+                  hero: Character
+                }
+                
+                type Character {
+                  name: String
+                  friends: [Character]
+                  homeWorld: Planet
+                  species: Species
+                }
+                
+                type Planet {
+                  name: String
+                  climate: String
+                }
+                
+                type Species {
+                  name: String
+                  lifespan: Int
+                  origin: Planet
+                }"
+                />
+              </div>
+            </Column>
+            <Column className="col col-md-4 col-sm-6 col-xs-12">
+              <br />
+              <h3>Evolve your API without versions</h3>
+              <br />
+              <p>
+                Add new fields and types to your GraphQL API without impacting existing queries.
+                Aging fields can be deprecated and hidden from tools. By using a single evolving
+                version, GraphQL APIs give apps continuous access to new features and encourage
+                cleaner, more maintainable server code.
+              </p>
+            </Column>
+          </Row>
+        </Section>
+        <Section>
+          <Row className="row mt-5 mb-5 Code_section_vertical">
+            <Column className="col col-md-4 col-sm-6 col-xs-12">
+              <br />
+              <h3>Bring your own data and code</h3>
+              <br />
+              <p>
+                GraphQL creates a uniform API across your entire application without being limited
+                by a specific storage engine. Write GraphQL APIs that leverage your existing data
+                and code with GraphQL engines available in many languages. You provide functions for
+                each field in the type system, and GraphQL calls them with optimal concurrency.
+              </p>
+            </Column>
+            <Column className="col col-md-6 col-sm-6 col-xs-12">
+              <div className="Code_container">
+                <GraphqlCodeBlock
+                  className="GraphqlCodeBlock"
+                  queryBody="{
+                  hero {
+                    name
+                    height
+                    mass
+                  }
+                }"
+                />
+                <GraphqlCodeBlock
+                  className="GraphqlCodeBlock border_separator"
+                  queryBody="type Query {
+                hero: Character
+              }
+              
+              type Character {
+                name: String
+                friends: [Character]
+                homeWorld: Planet
+                species: Species
+              }
+              
+              type Planet {
+                name: String
+                climate: String
+              }
+              
+              type Species {
+                name: String
+                lifespan: Int
+                origin: Planet
+              }"
+                />
+              </div>
+            </Column>
+          </Row>
+        </Section>
+        <Section gray title="Who’s using GraphQL?">
           <Container>
-            <BuiltWith companies={frameworks} gray />
+            <p>
+              Facebook's mobile apps have been powered by GraphQL since 2012. A GraphQL spec was
+              open sourced in 2015 and is now available in many environments and used by teams of
+              all sizes.
+            </p>
+            <BuiltWith companies={frameworks} />
           </Container>
         </Section>
+        <Footer>
+          <Container>
+            <Row>
+              <Column>
+                <img src="https://graphql.org/img/logo-gray.svg" width={60} alt="GraphQL logo" />
+              </Column>
+              <Column>
+                <ul className="list-group">
+                  <li className="list-group-item disabled">Learn</li>
+                  <li className="list-group-item">Introduction</li>
+                  <li className="list-group-item">Query Language</li>
+                  <li className="list-group-item">Type System</li>
+                  <li className="list-group-item">Execution</li>
+                  <li className="list-group-item">Best Practices</li>
+                </ul>
+              </Column>
+              <Column>
+                <ul className="list-group">
+                  <li className="list-group-item disabled">Learn</li>
+                  <li className="list-group-item">Introduction</li>
+                  <li className="list-group-item">Query Language</li>
+                  <li className="list-group-item">Type System</li>
+                  <li className="list-group-item">Execution</li>
+                  <li className="list-group-item">Best Practices</li>
+                </ul>
+              </Column>
+              <Column>
+                <ul className="list-group">
+                  <li className="list-group-item disabled">Learn</li>
+                  <li className="list-group-item">Introduction</li>
+                  <li className="list-group-item">Query Language</li>
+                  <li className="list-group-item">Type System</li>
+                  <li className="list-group-item">Execution</li>
+                  <li className="list-group-item">Best Practices</li>
+                </ul>
+              </Column>
+            </Row>
+            <Row>
+              <Column>
+                <Copyright text="Front10, Inc" showAllRightText={false} />
+              </Column>
+            </Row>
+          </Container>
+        </Footer>
       </div>
     </div>
   </ThemeSelector>

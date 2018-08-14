e4478f85c0a44ee32e058ba6f351cfd5747a7909
Yoandry <systems@front10.com>
2018-08-14 11:23:11 -0400

    Donations JSX addons

--- src/stories/components/donations/index.js
+++ src/stories/components/donations/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { setAddon, storiesOf } from '@storybook/react';
 import { withKnobs, text } from '@storybook/addon-knobs/react';
 import withReadme from 'storybook-readme/with-readme';
+import JSXAddon from 'storybook-addon-jsx';
 import { ThemeSelector } from '../../../addons/ThemeSwitcher';
-
+import jsxConfig from '../../mock/jsxConfig';
 import { Donation } from '../../../components';
 import Readme from '../../../components/Donation/README.md';
 
+setAddon(JSXAddon);
 const stories = storiesOf('Components', module);
 
 stories.addDecorator(withKnobs);
 
-stories.add(
+stories.addWithJSX(
   'Donation',
   withReadme([Readme], () => (
     <ThemeSelector>
@@ -22,5 +24,6 @@ stories.add(
         imageAlt={text('Image alt', 'Open collective logo')}
       />
     </ThemeSelector>
-  ))
+  )),
+  jsxConfig
 );

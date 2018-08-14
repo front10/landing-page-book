37e4eeac2c70f19d757d597ac29ea3bbc93e5083
Yulier Camilo Garrido Garrido <ycgarrido@gmail.com>
2018-08-14 10:39:57 -0400

    Create navbar stories

--- src/stories/components/navbar/index.js
+++ src/stories/components/navbar/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { setAddon, storiesOf } from '@storybook/react';
 import { withKnobs, text, select, boolean } from '@storybook/addon-knobs/react';
 import withReadme from 'storybook-readme/with-readme';
+import JSXAddon from 'storybook-addon-jsx';
+
+import jsxConfig from '../../mock/jsxConfig';
+
 import { ThemeSelector } from '../../../addons/ThemeSwitcher';
 
-import { Navbar, NavbarNav, Input, Button, NavbarLink, Icon } from '../../../components';
+import {
+  Navbar,
+  NavbarNav,
+  Input,
+  LanguageSwitcher,
+  NavbarLink,
+  Icon,
+  Container,
+  NavbarCollapse,
+  NavbarBrand,
+  Image
+} from '../../../components';
 import Readme from '../../../components/Navbar/README.md';
+
+setAddon(JSXAddon);
 
 const stories = storiesOf('Components/Navbar', module);
 
@@ -31,29 +48,18 @@ const rightItems = [
     title: 'Services'
   },
   {
-    title: 'What we do'
-  },
-  {
-    title: 'Process'
-  },
-  {
-    title: 'Founders'
-  },
-  {
     title: 'Contact'
   }
 ];
 
-const onItemClick = () => {};
-
-stories.add(
+stories.addWithJSX(
   'Default',
   withReadme([Readme], () => (
     <ThemeSelector>
       <Navbar
         transparent={boolean('Transparent', false)}
         fixed={boolean('Fixed', false)}
-        companyName={text('Company name', 'Front10')}
+        companyName={text('Company name', '')}
         companyLink={text('Company link', 'https://front10.com')}
         companyLogo={text('Company logo', 'images/logo/front10.png')}
         expand={select(
@@ -67,15 +73,138 @@ stories.add(
         )}
         rightItems={rightItems}
         leftItems={leftItems}
-        onItemClick={onItemClick}
         className="mb-2"
       />
     </ThemeSelector>
-  ))
+  )),
+  jsxConfig
 );
 
-stories.add(
-  'Children',
+stories.addWithJSX(
+  'Mode container',
+  withReadme([Readme], () => (
+    <ThemeSelector>
+      <Navbar expand="md">
+        <Container>
+          <NavbarCollapse>
+            <NavbarBrand>
+              <Image
+                width="120"
+                src={text(
+                  'Company logo',
+                  'https://webpack.js.org/e0b5805d423a4ec9473ee315250968b2.svg'
+                )}
+                alt="Webpack logo"
+              />
+            </NavbarBrand>
+            <NavbarNav alignItems="right">
+              <NavbarLink>Documentation</NavbarLink>
+              <NavbarLink>Contribute</NavbarLink>
+              <NavbarLink>Vote</NavbarLink>
+              <NavbarLink>Blog</NavbarLink>
+              <NavbarLink>
+                <Icon icon="fa fa-search" />
+              </NavbarLink>
+              <NavbarLink>
+                <Icon icon="fa fa-github" />
+              </NavbarLink>
+              <NavbarLink>
+                <Icon icon="fa fa-stack-overflow" />
+              </NavbarLink>
+              <LanguageSwitcher showArrow={false} />
+            </NavbarNav>
+          </NavbarCollapse>
+        </Container>
+      </Navbar>
+    </ThemeSelector>
+  )),
+  jsxConfig
+);
+
+stories.addWithJSX(
+  'Reversed',
+  withReadme([Readme], () => (
+    <ThemeSelector>
+      <Navbar expand="md">
+        <Container>
+          <NavbarCollapse>
+            <NavbarNav>
+              <LanguageSwitcher showArrow={false} />
+              <NavbarLink>
+                <Icon icon="fa fa-stack-overflow" />
+              </NavbarLink>
+              <NavbarLink>
+                <Icon icon="fa fa-github" />
+              </NavbarLink>
+              <NavbarLink>
+                <Icon icon="fa fa-search" />
+              </NavbarLink>
+              <NavbarLink>Blog</NavbarLink>
+              <NavbarLink>Vote</NavbarLink>
+              <NavbarLink>Contribute</NavbarLink>
+              <NavbarLink>Documentation</NavbarLink>
+            </NavbarNav>
+            <NavbarNav alignItems="right">
+              <NavbarBrand>
+                <Image
+                  width="120"
+                  src={text(
+                    'Company logo',
+                    'https://webpack.js.org/e0b5805d423a4ec9473ee315250968b2.svg'
+                  )}
+                  alt="Webpack logo"
+                />
+              </NavbarBrand>
+            </NavbarNav>
+          </NavbarCollapse>
+        </Container>
+      </Navbar>
+    </ThemeSelector>
+  )),
+  jsxConfig
+);
+
+stories.addWithJSX(
+  'Via children',
+  withReadme([Readme], () => (
+    <ThemeSelector>
+      <Navbar expand="md">
+        <NavbarCollapse>
+          <NavbarBrand>
+            <Image
+              width="120"
+              src={text(
+                'Company logo',
+                'https://webpack.js.org/e0b5805d423a4ec9473ee315250968b2.svg'
+              )}
+              alt="Webpack logo"
+            />
+          </NavbarBrand>
+          <NavbarNav alignItems="right">
+            <NavbarLink>Documentation</NavbarLink>
+            <NavbarLink>Contribute</NavbarLink>
+            <NavbarLink>Vote</NavbarLink>
+            <NavbarLink>Blog</NavbarLink>
+            <NavbarLink>
+              <Icon icon="fa fa-search" />
+            </NavbarLink>
+            <NavbarLink>
+              <Icon icon="fa fa-github" />
+            </NavbarLink>
+            <NavbarLink>
+              <Icon icon="fa fa-stack-overflow" />
+            </NavbarLink>
+            <LanguageSwitcher showArrow={false} />
+          </NavbarNav>
+        </NavbarCollapse>
+      </Navbar>
+    </ThemeSelector>
+  )),
+  jsxConfig
+);
+
+stories.addWithJSX(
+  'With search',
   withReadme([Readme], () => (
     <ThemeSelector>
       <Navbar
@@ -94,23 +223,54 @@ stories.add(
           'md'
         )}
       >
-        <NavbarNav>
-          <NavbarLink>
-            <Icon className="mr-1" icon="fa fa-google" />
-            Google
-          </NavbarLink>
-          <NavbarLink>
-            <Icon className="mr-1" icon="fa fa-facebook-official " />
-            Facebook
-          </NavbarLink>
+        <NavbarNav alignItems="right">
+          <Input placeholder="Search" icon="fa fa-search" />
         </NavbarNav>
-        <form className="form-inline mt-2 mt-md-0">
-          <Input className="mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-          <Button className="btn-outline-warning my-2 my-sm-0" type="submit">
-            {'Search'}
-          </Button>
-        </form>
       </Navbar>
     </ThemeSelector>
-  ))
+  )),
+  jsxConfig
+);
+
+stories.addWithJSX(
+  'With user photo',
+  withReadme([Readme], () => (
+    <ThemeSelector>
+      <Navbar expand="md">
+        <NavbarCollapse>
+          <NavbarBrand>
+            <Image
+              width="120"
+              src={text(
+                'Company logo',
+                'https://webpack.js.org/e0b5805d423a4ec9473ee315250968b2.svg'
+              )}
+              alt="Webpack logo"
+            />
+          </NavbarBrand>
+          <NavbarNav alignItems="right">
+            <NavbarLink>
+              <Icon icon="fa fa-search" />
+            </NavbarLink>
+            <NavbarLink>
+              <Icon icon="fa fa-github" />
+            </NavbarLink>
+            <NavbarLink>
+              <Icon icon="fa fa-stack-overflow" />
+            </NavbarLink>
+            <LanguageSwitcher showArrow={false} />
+            <NavbarLink href="https://twitter.com/ycgarrido">
+              <Image
+                rounded
+                width="30"
+                src="https://pbs.twimg.com/profile_images/985858821850415104/88svfp18_400x400.jpg"
+              />
+              ycgarrido
+            </NavbarLink>
+          </NavbarNav>
+        </NavbarCollapse>
+      </Navbar>
+    </ThemeSelector>
+  )),
+  jsxConfig
 );
--- src/stories/pages/front10/index.js
+++ src/stories/pages/front10/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { setAddon, storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs/react';
+import JSXAddon from 'storybook-addon-jsx';
 import { ThemeSelector } from '../../../addons/ThemeSwitcher';
+
+import jsxConfig from '../../mock/jsxConfig';
 
 import {
   Navbar,
@@ -85,60 +88,66 @@ const buttons = [
   }
 ];
 
+setAddon(JSXAddon);
+
 const stories = storiesOf('Pages', module);
 
 stories.addDecorator(withKnobs);
 
-stories.add('Front10', () => (
-  <ThemeSelector>
-    <Navbar
-      companyName="Front10"
-      companyLink="https://front10.com"
-      companyLogo="images/logo/front10.png"
-      rightItems={rightItems}
-      expand="md"
-    />
-    <Hero
-      overlayColor="#383d45"
-      opacity={1}
-      subHeaderPosition="top"
-      subHeader="Leverage your process!"
-      header="REACT & ANGULARJS COMPONENTS"
-      buttons={buttons}
-    />
-    <div className="mt-5">
-      <Section title="OUR SERVICES" subTitle="We do scalable Front-End development 3x faster.">
-        <Team members={services} imageBorder showBorder={false} />
-      </Section>
-      <Section gray title="WHAT WE DO" subTitle="One Look Is Worth A Thousand Words.">
-        <Container>
-          <Video source="https://youtu.be/dCrLwWdju68" />
-        </Container>
-      </Section>
-      <Section title="OUR FOUNDERS" subTitle="Dariel & Alberto.">
-        <Container>
-          <Team
-            members={team}
-            imageBorder
-            showBorder={false}
-            socials={['twitter', 'facebook', 'linkedin']}
-            socialGray
-          />
-        </Container>
-      </Section>
-      <Section gray>
-        <Container>
-          <BuiltWith companies={frameworks} gray />
-        </Container>
-      </Section>
+stories.addWithJSX(
+  'Front10',
+  () => (
+    <ThemeSelector>
+      <Navbar
+        companyName="Front10"
+        companyLink="https://front10.com"
+        companyLogo="images/logo/front10.png"
+        rightItems={rightItems}
+        expand="md"
+      />
       <Hero
-        isFixed={false}
-        image="images/hero/map-image.png"
-        particles={false}
-        backgroundColor="#383d45"
-      >
-        <ContactUs />
-      </Hero>
-    </div>
-  </ThemeSelector>
-));
+        overlayColor="#383d45"
+        opacity={1}
+        subHeaderPosition="top"
+        subHeader="Leverage your process!"
+        header="REACT & ANGULARJS COMPONENTS"
+        buttons={buttons}
+      />
+      <div className="mt-5">
+        <Section title="OUR SERVICES" subTitle="We do scalable Front-End development 3x faster.">
+          <Team members={services} imageBorder showBorder={false} />
+        </Section>
+        <Section gray title="WHAT WE DO" subTitle="One Look Is Worth A Thousand Words.">
+          <Container>
+            <Video source="https://youtu.be/dCrLwWdju68" />
+          </Container>
+        </Section>
+        <Section title="OUR FOUNDERS" subTitle="Dariel & Alberto.">
+          <Container>
+            <Team
+              members={team}
+              imageBorder
+              showBorder={false}
+              socials={['twitter', 'facebook', 'linkedin']}
+              socialGray
+            />
+          </Container>
+        </Section>
+        <Section gray>
+          <Container>
+            <BuiltWith companies={frameworks} gray />
+          </Container>
+        </Section>
+        <Hero
+          isFixed={false}
+          image="images/hero/map-image.png"
+          particles={false}
+          backgroundColor="#383d45"
+        >
+          <ContactUs />
+        </Hero>
+      </div>
+    </ThemeSelector>
+  ),
+  jsxConfig
+);

6de141af5c19889d257a6e128bc98379174e9d8d
front10-circleci <systems@front10.com>
2018-09-24 13:59:57 -0400

    new version off travis conf

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -6,28 +6,32 @@ if [[ $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == "false" ]]; then
 
 echo "Starting to update gh-pages\n"
 
-#copy data we're interested in to other place
-cp -R dist $HOME/dist
+git config --global user.email $GH_EMAIL
+git config --global user.name $GH_NAME
+remote=$(git config remote.origin.url)
+git clone $remote
 
-#go to home and setup git
-cd $HOME
-git config --global user.email "systems@front10.com"
-git config --global user.name "front10-circleci"
+cd landing-page-book
 
-#using token clone gh-pages branch
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git gh-pages > /dev/null
+git checkout --orphan gh-pages
 
-#go into directory and copy data we're interested in to that directory
-cd gh-pages
-cp -Rf $HOME/dist/* .
+git rm -rf --cached . &&
+sudo npm update -g npm@latest &&
+npm install &&
+npm run eslint &&
+npm run test -- -u &&
+npm run build-storybook &&  
 
-echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
-echo "[View live](https://${GH_USER}.github.io/${GH_REPO}/)" > README.md
+find . | grep -v "public" | grep -v ".git"| grep -v "."| grep -v ".." | xargs rm -rf &&
 
-#add, commit and push files
-git add -f .
+mv public/* . &&
+
+rm -rf public &&
+
+git add -A .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-git push -fq origin gh-pages > /dev/null
+
+git push --force --quiet origin gh-pages
 
 echo "Done updating gh-pages\n"
 

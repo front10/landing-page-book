71610f23aad578a3c9761246e959dccd449a1c36
front10-circleci <systems@front10.com>
2018-09-27 16:08:29 -0400

    Updates

--- src/service/constant.js
+++
@@ -1,2 +0,0 @@
-const GITHUB_API_URL = 'https://api.github.com/';
-export default GITHUB_API_URL;
--- storybook-utils/decorators/paddingDecorator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const styles = {
-  padding: '20px'
-};
-
-export default function(storyFn) {
-  return <div style={styles}>{storyFn()}</div>;
-}

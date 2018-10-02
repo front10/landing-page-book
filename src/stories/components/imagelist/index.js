31da821d4c493a3760ec178885ec126a340f659f
Yulier Camilo Garrido Garrido <ycgarrido@gmail.com>
2018-10-02 12:22:52 -0400

    Change images from ImageList component

--- src/stories/components/imagelist/index.js
+++ src/stories/components/imagelist/index.js
@@ -14,100 +14,28 @@ setAddon(JSXAddon);
 
 const companies = [
   {
-    image: 'https://cdn.designcrowd.com/blog/2016/January/top-company-logos-black/4_Nike_400.png'
+    image: 'images/image-list/react.png'
   },
   {
-    image: 'https://qph.fs.quoracdn.net/main-qimg-5f4deb2eafbed371d07b24cfe4a11f6e'
+    image: 'images/image-list/graphql.png'
   },
   {
-    image: 'http://iol13.linguistics-bg.com/wp-content/uploads/2015/05/Coca-Cola-Logo-Red.png'
+    image: 'images/image-list/apollo.png'
   },
   {
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQizYke0pb5GJsX7q7EhWSIKAcGNZv53eI2H_TTPfF3p5IKNpx'
+    image: 'images/image-list/storybook.png'
   },
   {
-    image: 'https://i.pinimg.com/originals/de/fd/43/defd4347f12a7fa7bbfa192be11532d6.png'
+    image: 'images/image-list/angularjs.png'
   },
   {
-    image: 'https://cdn.designcrowd.com/blog/2016/January/top-company-logos-black/4_Nike_400.png'
+    image: 'images/image-list/bootstrap.png'
   },
   {
-    image: 'https://qph.fs.quoracdn.net/main-qimg-5f4deb2eafbed371d07b24cfe4a11f6e'
+    image: 'images/image-list/browserstack.png'
   },
   {
-    image: 'http://iol13.linguistics-bg.com/wp-content/uploads/2015/05/Coca-Cola-Logo-Red.png'
-  },
-  {
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQizYke0pb5GJsX7q7EhWSIKAcGNZv53eI2H_TTPfF3p5IKNpx'
-  },
-  {
-    image: 'https://i.pinimg.com/originals/de/fd/43/defd4347f12a7fa7bbfa192be11532d6.png'
-  },
-  {
-    image: 'https://cdn.designcrowd.com/blog/2016/January/top-company-logos-black/4_Nike_400.png'
-  },
-  {
-    image: 'https://qph.fs.quoracdn.net/main-qimg-5f4deb2eafbed371d07b24cfe4a11f6e'
-  },
-  {
-    image: 'http://iol13.linguistics-bg.com/wp-content/uploads/2015/05/Coca-Cola-Logo-Red.png'
-  },
-  {
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQizYke0pb5GJsX7q7EhWSIKAcGNZv53eI2H_TTPfF3p5IKNpx'
-  },
-  {
-    image: 'https://i.pinimg.com/originals/de/fd/43/defd4347f12a7fa7bbfa192be11532d6.png'
-  },
-  {
-    image: 'https://cdn.designcrowd.com/blog/2016/January/top-company-logos-black/4_Nike_400.png'
-  },
-  {
-    image: 'https://qph.fs.quoracdn.net/main-qimg-5f4deb2eafbed371d07b24cfe4a11f6e'
-  },
-  {
-    image: 'http://iol13.linguistics-bg.com/wp-content/uploads/2015/05/Coca-Cola-Logo-Red.png'
-  },
-  {
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQizYke0pb5GJsX7q7EhWSIKAcGNZv53eI2H_TTPfF3p5IKNpx'
-  },
-  {
-    image: 'https://i.pinimg.com/originals/de/fd/43/defd4347f12a7fa7bbfa192be11532d6.png'
-  },
-  {
-    image: 'https://cdn.designcrowd.com/blog/2016/January/top-company-logos-black/4_Nike_400.png'
-  },
-  {
-    image: 'https://qph.fs.quoracdn.net/main-qimg-5f4deb2eafbed371d07b24cfe4a11f6e'
-  },
-  {
-    image: 'http://iol13.linguistics-bg.com/wp-content/uploads/2015/05/Coca-Cola-Logo-Red.png'
-  },
-  {
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQizYke0pb5GJsX7q7EhWSIKAcGNZv53eI2H_TTPfF3p5IKNpx'
-  },
-  {
-    image: 'https://i.pinimg.com/originals/de/fd/43/defd4347f12a7fa7bbfa192be11532d6.png'
-  },
-  {
-    image: 'https://cdn.designcrowd.com/blog/2016/January/top-company-logos-black/4_Nike_400.png'
-  },
-  {
-    image: 'https://qph.fs.quoracdn.net/main-qimg-5f4deb2eafbed371d07b24cfe4a11f6e'
-  },
-  {
-    image: 'http://iol13.linguistics-bg.com/wp-content/uploads/2015/05/Coca-Cola-Logo-Red.png'
-  },
-  {
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQizYke0pb5GJsX7q7EhWSIKAcGNZv53eI2H_TTPfF3p5IKNpx'
-  },
-  {
-    image: 'https://i.pinimg.com/originals/de/fd/43/defd4347f12a7fa7bbfa192be11532d6.png'
+    image: 'images/image-list/jest.png'
   }
 ];
 

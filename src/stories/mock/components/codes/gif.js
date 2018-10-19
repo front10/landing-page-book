21ac9f702bd7abfd99e670308c787d6c499d6e0a
ycbarrosocu <ycbarrosocu@gmail.com>
2018-10-19 10:19:28 -0400

    Added code to Gif stories

---
+++ src/stories/mock/components/codes/gif.js
@@ -0,0 +1,9 @@
+const def = ` <Gif
+    image={text('Image', 'images/gif/write.gif')}
+    preview={text('Preview', 'images/demo/develop.png')}/>`;
+const autoplay = ` <Gif
+    image={text('Image', 'images/gif/write.gif')}
+    preview={text('Preview', 'images/demo/develop.png')}
+    autoplay/>`;
+
+export { def, autoplay };

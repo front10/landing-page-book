e79a9f552db918d176c6276c43ecf1f801eaa716
Jorge Luis Suárez Falcón <jlfalcon3589@gmail.com>
2018-10-22 17:17:24 -0400

    Changed story in the Navbar component.

--- src/stories/components/link/index.js
+++ src/stories/components/link/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import { setAddon, storiesOf } from '@storybook/react';
-import { withKnobs, text, boolean, select } from '@storybook/addon-knobs/react';
+import { storiesOf } from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
-import JSXAddon from 'storybook-addon-jsx';
 import StoryHeader from '../../../../storybook-utils/components/StoryHeader';
-
-import jsxConfig from '../../mock/jsxConfig';
+import PropsManager from '../../../../storybook-utils/components/PropsManager';
 import { ThemeSelector } from '../../../addons/ThemeSwitcher';
-
-import { Link, Icon, Code } from '../../../components';
+import { Link, Icon } from '../../../components';
 import Readme from '../../../components/Link/README.md';
-import def from '../../mock/components/codes/link';
-
-setAddon(JSXAddon);
 
 const stories = storiesOf('Elements', module);
-
-stories.addDecorator(withKnobs);
 
 stories.addWithJSX(
   'Link',
@@ -26,37 +17,12 @@ stories.addWithJSX(
         name="Link"
         description="Link are hyperlink. You can click on a link and jump to another document. When you move the mouse over a link, the mouse arrow will turn into a little hand."
       />
-      <Link
-        disabled={boolean('Disabled', false)}
-        href={text('Link', 'https://front10.com')}
-        className={text('Class name', '')}
-        tooltip={text('Tooltip', 'This is a tooltip')}
-        target={select(
-          'Target',
-          {
-            '': '',
-            _blank: '_blank',
-            _parent: '_parent',
-            _self: '_self',
-            _top: '_top'
-          },
-          ''
-        )}
-      >
-        <Icon icon="fa fa-home" />
-        <span>This is a link</span>
-      </Link>
-      <div className="mt-4">
-        <Code
-          code={def}
-          languageCode="jsx"
-          readOnly
-          collapsible
-          collapsed
-          showDeleteButton={false}
-        />
-      </div>
+      <PropsManager scope={{ React, Link, Icon }}>
+        <Link href="https://front10.com" tooltip="This is a tooltip" target="_blank">
+          <Icon icon="fa fa-home" />
+          <span>This is a link</span>
+        </Link>
+      </PropsManager>
     </ThemeSelector>
-  )),
-  jsxConfig
+  ))
 );

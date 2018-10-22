48d55e4a344e5119b4229606e5ecbde6b19143e4
Yulier Camilo Garrido Garrido <ycgarrido@gmail.com>
2018-10-22 17:40:34 -0400

    Add code to all stories

--- src/stories/components/contact-info/index.js
+++ src/stories/components/contact-info/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { setAddon, storiesOf } from '@storybook/react';
-import { withKnobs, text, select, boolean, number } from '@storybook/addon-knobs/react';
+import { storiesOf } from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
-import JSXAddon from 'storybook-addon-jsx';
 import { ThemeSelector } from '../../../addons/ThemeSwitcher';
-import jsxConfig from '../../mock/jsxConfig';
-import { ContactInfo, Row, Column, Container, Code } from '../../../components';
+import { ContactInfo, Row, Column, Container } from '../../../components';
 import Readme from '../../../components/ContactInfo/README.md';
 import StoryHeader from '../../../../storybook-utils/components/StoryHeader';
-import def from '../../mock/components/codes/contactinfo';
+import PropsManager from '../../../../storybook-utils/components/PropsManager';
 
-setAddon(JSXAddon);
 const stories = storiesOf('Components/ContactInfo', module);
 
-stories.addDecorator(withKnobs);
-
-stories.addWithJSX(
+stories.add(
   'Default',
   withReadme([Readme], () => (
     <ThemeSelector>
@@ -24,40 +18,27 @@ stories.addWithJSX(
         description="It is always important to have the most information of your contacts, our component provides it"
       />
       <Container>
-        <Row>
-          <Column className="col col-md-5">
-            <ContactInfo
-              showBorder={boolean('Show border', true)}
-              showIcons={boolean('Show icons', true)}
-              lat={number('Latitude', 26.5)}
-              lng={number('Longitude', -80.2)}
-              locationApiKey="AIzaSyCnbxTadExb8UYwzkBkQ_ITf5gFL_3jaAc"
-              address={text('Address', '9728 Birchwood St. Navarre, FL 32566')}
-              email={text('Email', 'contact@front10.com')}
-              phone={text('Phone', '+001 3521 4587')}
-              mobile={text('Mobile', '+001 3521 4587')}
-              fax={text('Fax', '208-658744')}
-              website={text('Website', 'www.front10.com')}
-              contentAlign={select(
-                'Content align',
-                { left: 'left', center: 'center', right: 'right' },
-                'left'
-              )}
-            />
-          </Column>
-        </Row>
+        <PropsManager scope={{ React, ContactInfo, Row, Column }}>
+          <Row>
+            <Column className="col col-md-5">
+              <ContactInfo
+                showBorder
+                showIcons
+                lat={26.5}
+                lng={-80.2}
+                locationApiKey="AIzaSyCnbxTadExb8UYwzkBkQ_ITf5gFL_3jaAc"
+                address="9728 Birchwood St. Navarre, FL 32566"
+                email="contact@front10.com"
+                phone="+001 3521 4587"
+                mobile="+001 3521 4587"
+                fax="208-658744"
+                website="www.front10.com"
+                contentAlign="left"
+              />
+            </Column>
+          </Row>
+        </PropsManager>
       </Container>
-      <div className="mt-4">
-        <Code
-          code={def}
-          languageCode="jsx"
-          readOnly
-          collapsible
-          collapsed
-          showDeleteButton={false}
-        />
-      </div>
     </ThemeSelector>
-  )),
-  jsxConfig
+  ))
 );
--- src/stories/components/gif/index.js
+++ src/stories/components/gif/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { setAddon, storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs/react';
+import { storiesOf } from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
-import JSXAddon from 'storybook-addon-jsx';
 import StoryHeader from '../../../../storybook-utils/components/StoryHeader';
+import PropsManager from '../../../../storybook-utils/components/PropsManager';
 import { ThemeSelector } from '../../../addons/ThemeSwitcher';
-import jsxConfig from '../../mock/jsxConfig';
-import { Gif, Code } from '../../../components';
+import { Gif } from '../../../components';
 import Readme from '../../../components/Gif/README.md';
-import { def, autoplay } from '../../mock/components/codes/gif';
 
-setAddon(JSXAddon);
 const stories = storiesOf('Components/Gif', module);
-
-stories.addDecorator(withKnobs);
 
 stories.addWithJSX(
   'Default',
@@ -23,23 +17,11 @@ stories.addWithJSX(
         name="Gif"
         description="A lossless format for image files that supports both animated and static images, Its one of the most used image format on web."
       />
-      <Gif
-        image={text('Image', 'images/gif/write.gif')}
-        preview={text('Preview', 'images/demo/develop.png')}
-      />
-      <div className="mt-4">
-        <Code
-          code={def}
-          languageCode="jsx"
-          readOnly
-          collapsible
-          collapsed
-          showDeleteButton={false}
-        />
-      </div>
+      <PropsManager scope={{ React, Gif }}>
+        <Gif image="images/gif/write.gif" preview="images/demo/develop.png" />
+      </PropsManager>
     </ThemeSelector>
-  )),
-  jsxConfig
+  ))
 );
 
 stories.addWithJSX(
@@ -51,22 +33,9 @@ stories.addWithJSX(
         storyName="With Autoplay"
         description="A lossless format for image files that supports both animated and static images, Its one of the most used image format on web."
       />
-      <Gif
-        image={text('Image', 'images/gif/write.gif')}
-        preview={text('Preview', 'images/demo/develop.png')}
-        autoplay
-      />
-      <div className="mt-4">
-        <Code
-          code={autoplay}
-          languageCode="jsx"
-          readOnly
-          collapsible
-          collapsed
-          showDeleteButton={false}
-        />
-      </div>
+      <PropsManager scope={{ React, Gif }}>
+        <Gif image="images/gif/write.gif" preview="images/demo/develop.png" autoplay />
+      </PropsManager>
     </ThemeSelector>
-  )),
-  jsxConfig
+  ))
 );

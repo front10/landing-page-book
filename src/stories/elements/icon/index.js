47839c20c36b0cb00e0c689f1219e2a1754ea6dd
Jorge Luis Suárez Falcón <jlfalcon3589@gmail.com>
2018-10-23 11:21:58 -0400

    Update missing libraries in the package json. Updated the PropManager in the stories of the elements.

--- src/stories/elements/icon/index.js
+++ src/stories/elements/icon/index.js
@@ -1,166 +1,95 @@
 import React from 'react';
-import { setAddon, storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs/react';
+import { storiesOf } from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
-import JSXAddon from 'storybook-addon-jsx';
 import StoryHeader from '../../../../storybook-utils/components/StoryHeader';
-
-import jsxConfig from '../../mock/jsxConfig';
+import PropsManager from '../../../../storybook-utils/components/PropsManager';
 import { ThemeSelector } from '../../../addons/ThemeSwitcher';
-import def from '../../mock/components/codes/icon';
 
-import { Icon, Code, Column, Row, Container } from '../../../components';
+import { Icon, Column, Row, Container } from '../../../components';
 import Readme from '../../../components/Icon/README.md';
-
-setAddon(JSXAddon);
 
 const stories = storiesOf('Elements/Icon', module);
 
-stories.addDecorator(withKnobs);
-
-stories.addWithJSX(
+stories.add(
   'Basic',
   withReadme([Readme], () => (
     <ThemeSelector>
       <StoryHeader name="Icon" description="Icon element." />
-      <Container>
-        <Row>
-          <Column className="text-center">
-            <Icon
-              icon={text('Icon', 'fa fa-cubes')}
-              className={text('Class name', 'text-secondary fa-3x')}
-            />
-          </Column>
-        </Row>
-      </Container>
-      <div className="mt-4">
-        <Code
-          code={def}
-          languageCode="jsx"
-          readOnly
-          collapsible
-          collapsed
-          showDeleteButton={false}
-        />
-      </div>
+      <PropsManager scope={{ React, Row, Column, Container, Icon }}>
+        <Container>
+          <Row>
+            <Column className="text-center">
+              <Icon icon="fa fa-cubes" className="text-secondary fa-3x" />
+            </Column>
+          </Row>
+        </Container>
+      </PropsManager>
     </ThemeSelector>
-  )),
-  jsxConfig
+  ))
 );
 
-stories.addWithJSX(
+stories.add(
   'Different Sizes',
   withReadme([Readme], () => (
     <ThemeSelector>
       <StoryHeader name="Icon" description="Icon element." />
-      <Container>
-        <Row>
-          <Column className="text-center">
-            <Icon icon={text('Icon', 'fa fa-home')} className={text('Class name', 'fa-1x')} />
-            <Icon icon={text('Icon', 'fa fa-home')} className={text('Class name', 'fa-2x')} />
-            <Icon icon={text('Icon', 'fa fa-home')} className={text('Class name', 'fa-3x')} />
-            <Icon icon={text('Icon', 'fa fa-home')} className={text('Class name', 'fa-4x')} />
-            <Icon icon={text('Icon', 'fa fa-home')} className={text('Class name', 'fa-5x')} />
-          </Column>
-        </Row>
-      </Container>
-      <div className="mt-4">
-        <Code
-          code={def}
-          languageCode="jsx"
-          readOnly
-          collapsible
-          collapsed
-          showDeleteButton={false}
-        />
-      </div>
+      <PropsManager scope={{ React, Row, Column, Container, Icon }}>
+        <Container>
+          <Row>
+            <Column className="text-center">
+              <Icon icon="fa fa-home" className="fa-1x" />
+              <Icon icon="fa fa-home" className="fa-2x" />
+              <Icon icon="fa fa-home" className="fa-3x" />
+              <Icon icon="fa fa-home" className="fa-4x" />
+              <Icon icon="fa fa-home" className="fa-5x" />
+            </Column>
+          </Row>
+        </Container>
+      </PropsManager>
     </ThemeSelector>
-  )),
-  jsxConfig
+  ))
 );
 
-stories.addWithJSX(
+stories.add(
   'Different Colors',
   withReadme([Readme], () => (
     <ThemeSelector>
       <StoryHeader name="Icon" description="Icon element." />
-      <Container>
-        <Row>
-          <Column className="text-center">
-            <Icon
-              icon={text('Icon', 'fa fa-home')}
-              className={text('Class name', 'fa-1x text-primary')}
-            />
-            <Icon
-              icon={text('Icon', 'fa fa-home')}
-              className={text('Class name', 'fa-2x text-secondary')}
-            />
-            <Icon
-              icon={text('Icon', 'fa fa-home')}
-              className={text('Class name', 'fa-3x text-success')}
-            />
-            <Icon
-              icon={text('Icon', 'fa fa-home')}
-              className={text('Class name', 'fa-4x text-danger')}
-            />
-            <Icon
-              icon={text('Icon', 'fa fa-home')}
-              className={text('Class name', 'fa-5x text-warning')}
-            />
-          </Column>
-        </Row>
-      </Container>
-      <div className="mt-4">
-        <Code
-          code={def}
-          languageCode="jsx"
-          readOnly
-          collapsible
-          collapsed
-          showDeleteButton={false}
-        />
-      </div>
+      <PropsManager scope={{ React, Row, Column, Container, Icon }}>
+        <Container>
+          <Row>
+            <Column className="text-center">
+              <Icon icon="fa fa-home" className="fa-1x text-primary" />
+              <Icon icon="fa fa-home" className="fa-2x text-secondary" />
+              <Icon icon="fa fa-home" className="fa-3x text-success" />
+              <Icon icon="fa fa-home" className="fa-4x text-danger" />
+              <Icon icon="fa fa-home" className="fa-5x text-warning" />
+            </Column>
+          </Row>
+        </Container>
+      </PropsManager>
     </ThemeSelector>
-  )),
-  jsxConfig
+  ))
 );
 
-stories.addWithJSX(
+stories.add(
   'All Icons',
   withReadme([Readme], () => (
     <ThemeSelector>
       <StoryHeader name="Icon" description="Icon element." />
-      <Container>
-        <Row>
-          <Column className="text-left">
-            <Icon icon={text('Icon', 'fa fa-home')} className={text('Class name', 'fa-2x mr-3')} />
-            <Icon icon={text('Icon', 'fa fa-cubes')} className={text('Class name', 'fa-2x mr-3')} />
-            <Icon
-              icon={text('Icon', 'fa fa-shower')}
-              className={text('Class name', 'fa-2x mr-3')}
-            />
-            <Icon
-              icon={text('Icon', 'fa fa-user-o')}
-              className={text('Class name', 'fa-2x mr-3')}
-            />
-            <Icon
-              icon={text('Icon', 'fa fa-anchor')}
-              className={text('Class name', 'fa-2x mr-3')}
-            />
-          </Column>
-        </Row>
-      </Container>
-      <div className="mt-4">
-        <Code
-          code={def}
-          languageCode="jsx"
-          readOnly
-          collapsible
-          collapsed
-          showDeleteButton={false}
-        />
-      </div>
+      <PropsManager scope={{ React, Row, Column, Container, Icon }}>
+        <Container>
+          <Row>
+            <Column className="text-left">
+              <Icon icon="fa fa-home" className="fa-2x mr-3" />
+              <Icon icon="fa fa-cubes" className="fa-2x mr-3" />
+              <Icon icon="fa fa-shower" className="fa-2x mr-3" />
+              <Icon icon="fa fa-user-o" className="fa-2x mr-3" />
+              <Icon icon="fa fa-anchor" className="fa-2x mr-3" />
+            </Column>
+          </Row>
+        </Container>
+      </PropsManager>
     </ThemeSelector>
-  )),
-  jsxConfig
+  ))
 );

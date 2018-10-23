a54714010f66371c76d8d27d01f5c260f386635e
ycbarrosocu <ycbarrosocu@gmail.com>
2018-10-23 14:33:35 -0400

    Added size prop to input cmp

--- src/stories/elements/input/index.js
+++ src/stories/elements/input/index.js
@@ -31,3 +31,45 @@ stories.add(
     </ThemeSelector>
   ))
 );
+stories.add(
+  'Size',
+  withReadme([Readme], () => (
+    <ThemeSelector>
+      <StoryHeader
+        name="Input"
+        storyName="Size"
+        description="The Input specifies an input field where the user can enter data.
+      <Input> elements are used within a <form> element to declare input controls that allow users to input data.      
+      An input field can vary in many ways, depending on the type attribute."
+      />
+      <PropsManager scope={{ React, Input }}>
+        <div>
+          <div className="mb-2">
+            <Input
+              name="Large"
+              value="This is an large input"
+              placeholder="Enter a value"
+              icon="fa fa-user"
+              size="lg"
+            />
+          </div>
+          <div className="mb-2">
+            <Input
+              name="Normal"
+              value="This is an normal input"
+              placeholder="Enter a value"
+              icon="fa fa-user"
+            />
+          </div>
+          <Input
+            name="Small"
+            value="This is an small input"
+            placeholder="Enter a value"
+            icon="fa fa-user"
+            size="sm"
+          />
+        </div>
+      </PropsManager>
+    </ThemeSelector>
+  ))
+);

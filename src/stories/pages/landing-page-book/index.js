8fcc225da75e81729dd76f2aa4c1673061588b7f
Jorge Luis Suárez Falcón <jlfalcon3589@gmail.com>
2018-10-29 08:24:06 -0400

    Added GridComponent and SideBar Component for use in the landing-page-book

--- src/stories/pages/landing-page-book/index.js
+++ src/stories/pages/landing-page-book/index.js
@@ -3,10 +3,13 @@ import { storiesOf } from '@storybook/react';
 
 import { ThemeSelector } from '../../../addons/ThemeSwitcher';
 
-import DetailsComponent from '../../../../storybook-utils/components/DetailsComponent';
+// import DetailsComponent from '../../../../storybook-utils/components/DetailsComponent';
+import GridComponent from '../../../../storybook-utils/components/GridComponents';
+import SideBar from '../../../../storybook-utils/components/SideBar';
 import { Navbar, Footer } from '../../../components';
 // Import DATA
-import { navbarData, story, importCode } from './data/data';
+import { navbarData, elements } from './data/data';
+// import { story, importCode } from './data/data';
 
 const stories = storiesOf('Pages', module);
 
@@ -23,18 +26,20 @@ stories.add('Landing Page Book', () => (
         rightItems={navbarData.rightItems}
         leftItems={navbarData.leftItems}
       />
-      <DetailsComponent
+      <GridComponent elements={elements} />
+      {/* <DetailsComponent
         name="Card"
         linkGithub="https://github.com/front10/landing-page-book/tree/master/src/components/Card"
         description="Cards are surfaces that display content and actions on a single topic. They should be easy to scan for relevant and actionable information. Elements, like text and images, should be placed on them in a way that clearly indicates hierarchy."
         stories={story}
         importCode={importCode}
-      />
+      /> */}
       <Footer
         socialUrl="https://front10.com"
         copyright="Front10, LLC"
         socials={['facebook', 'linkedin', 'google', 'twitter']}
       />
     </div>
+    <SideBar />
   </ThemeSelector>
 ));

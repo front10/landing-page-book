d0ba7b45d4d4d5e83de2ee4992d80255d6bdab82
Admin <darielnoel@gmail.com>
2018-11-04 00:37:58 -0400

    Move functional CSS to the engine level for Header Component.

--- src/helpers/WithStyles/WithStyles.jsx
+++ src/helpers/WithStyles/WithStyles.jsx
@@ -33,17 +33,55 @@ const withStyles = WrappedComponent => {
       return colorBasedClasses;
     }
 
+    getBorderBasedStyles() {
+      const { border, borderTop, borderRight, borderBottom, borderLeft } = this.props;
+      let borderBasedClasses = '';
+      if (border) {
+        borderBasedClasses = `${borderBasedClasses} border`;
+      }
+      if (borderTop) {
+        borderBasedClasses = `${borderBasedClasses} border-top`;
+      }
+      if (borderRight) {
+        borderBasedClasses = `${borderBasedClasses} border-right`;
+      }
+      if (borderBottom) {
+        borderBasedClasses = `${borderBasedClasses} border-bottom`;
+      }
+      if (borderLeft) {
+        borderBasedClasses = `${borderBasedClasses} border-left`;
+      }
+      return borderBasedClasses;
+    }
+
+    getTextAlignBasedStyles() {
+      const { textAlign } = this.props;
+      let textAlignBasedClasses = '';
+      if (textAlign) {
+        textAlignBasedClasses = `${textAlignBasedClasses} text-${textAlign}`;
+      }
+      return textAlignBasedClasses;
+    }
+
     translatePropsStyles() {
       const { className, classNames } = this.props;
       let finalClasses = `${className} ${classNames}`;
       const layoutBasedClasses = this.getLayoutBasedStyles();
       const colorBasedClasses = this.getColorBasedStyles();
+      const borderBasedClasses = this.getBorderBasedStyles();
+      const textAlignBasedClasses = this.getTextAlignBasedStyles();
 
       if (layoutBasedClasses) {
         finalClasses = `${finalClasses} ${layoutBasedClasses}`;
       }
       if (colorBasedClasses) {
         finalClasses = `${finalClasses} ${colorBasedClasses}`;
+      }
+      if (borderBasedClasses) {
+        finalClasses = `${finalClasses} ${borderBasedClasses}`;
+      }
+      if (textAlignBasedClasses) {
+        finalClasses = `${finalClasses} ${textAlignBasedClasses}`;
       }
       return finalClasses;
     }
@@ -57,13 +95,31 @@ const withStyles = WrappedComponent => {
   Styled.propTypes = {
     margin: PropTypes.string,
     marginRight: PropTypes.string,
-    marginLeft: PropTypes.string
+    marginLeft: PropTypes.string,
+    color: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
+    border: PropTypes.string,
+    borderTop: PropTypes.string,
+    borderRight: PropTypes.string,
+    borderBottom: PropTypes.string,
+    borderLeft: PropTypes.string,
+    textAlign: PropTypes.string
   };
 
   Styled.defaultProps = {
     margin: null,
     marginRight: null,
-    marginLeft: null
+    marginLeft: null,
+    color: null,
+    bgColor: null,
+    textColor: null,
+    border: null,
+    borderTop: null,
+    borderRight: null,
+    borderBottom: null,
+    borderLeft: null,
+    textAlign: null
   };
   return Styled;
 };

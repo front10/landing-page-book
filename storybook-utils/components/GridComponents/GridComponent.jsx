866c748f38611e32dfdd28a70a52314ff62567fd
Jorge Luis Suárez Falcón <jlfalcon3589@gmail.com>
2018-11-06 16:45:45 -0500

    Fixed align tetx component.

--- storybook-utils/components/GridComponents/GridComponent.jsx
+++ storybook-utils/components/GridComponents/GridComponent.jsx
@@ -28,6 +28,7 @@ class GridComponent extends React.Component {
           <Column key={component} className="col-12 col-sm-6 col-md-4 col-lg-3">
             <Link href={`/components/${component.toLowerCase()}`} tooltip="View more details">
               <Card
+                textAlign="center"
                 border={false}
                 margin="2"
                 showBorder={false}

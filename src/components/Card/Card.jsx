2364939fa275f78ea3abadc59ed626902cbd57f1
Jorge Luis Suárez Falcón <jlfalcon3589@gmail.com>
2018-11-07 10:34:21 -0500

    Added padding style imagen in card component.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -31,7 +31,6 @@ class Card extends React.Component {
             src={image}
             shadow={imageShadow}
             alt="Generic placeholder"
-            className="p-3"
           />
         )}
         {(title || subTitle || summary) && (

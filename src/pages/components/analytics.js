9221ab5af3d6d7d1b3e5722a394614af8102b7a6
ycbarrosocu <ycbarrosocu@gmail.com>
2018-11-08 16:07:36 -0500

    Updating master

---
+++ src/pages/components/analytics.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import MainLayout from '../../gatsby-layouts/main';
+import DetailsComponent from '../../../storybook-utils/components/DetailsComponent';
+import component from '../../stories/mock/components/stories/analytics';
+import map from '../../components/Analytics/map.json';
+
+class ComponentView extends React.Component {
+  render() {
+    return (
+      <MainLayout pagePushedFunction={this.handlepushedPageLayout}>
+        {component && (
+          <DetailsComponent
+            name={component.name}
+            linkGithub="https://github.com/front10/landing-page-book/tree/master/src/components/Analytics"
+            description={component.summary}
+            stories={component.stories}
+            importCode={component.import}
+            withStyles={component.withStyles}
+            extractProps={component.extractProps}
+            propsDescription={map.props}
+          />
+        )}
+      </MainLayout>
+    );
+  }
+}
+
+export default ComponentView;
